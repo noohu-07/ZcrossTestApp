571a442814a129418ac17d87e0fa6f3e5cc9f5f1
noohu-07 <noohu@zumisolutions.com>
2025-09-25 11:50:10 +0530

    Intial Commit

--- app/src/main/java/com/noohu/zcrosstest/MainActivity.java
+++ app/src/main/java/com/noohu/zcrosstest/MainActivity.java
@@ -196,7 +196,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        /*Camera test activity*/
+        /*Camera test*/
 
         Camer_test = findViewById(R.id.Camer_test);
 
